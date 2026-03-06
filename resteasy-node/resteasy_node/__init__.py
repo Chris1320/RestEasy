30078d777f492ee0ccb2e9b4ee631e95deadfc46
Chris1320 <me@chrisfromnowhere.me>
2026-03-06 22:14:49 +0800

    feat(node): set up dependencies and `info.py`
    Signed-off-by: Chris1320 <me@chrisfromnowhere.me>

---
+++ resteasy-node/resteasy_node/__init__.py
@@ -0,0 +1,10 @@
+from resteasy_node.core.info import AUTHOR, DESCRIPTION, EMAIL, LICENSE, NAME, URL, VERSION
+
+__all__ = ["__version__", "__author__", "__email__", "__license__", "__description__", "__url__"]
+__author__ = AUTHOR
+__description__ = DESCRIPTION
+__email__ = EMAIL
+__license__ = LICENSE
+__name__ = NAME
+__url__ = URL
+__version__ = VERSION
---
+++ resteasy-node/resteasy_node/__main__.py
@@ -0,0 +1,8 @@
+from resteasy_node.core import info
+
+def main():
+    print(f"Starting {info.NAME} v {info.VERSION}")
+
+
+if __name__ == "__main__":
+    main()
